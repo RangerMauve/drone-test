cb654cb2a2361c0a8aab62baa6d9625a8999a168
RangerMauve <RangerMauve@hotmail.com>
2014-07-27 11:17:24 -0400

    Changed format

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
-var arDrone = require('ar-drone');
-var client = arDrone.createClient();
+var c = require('ar-drone').createClient();
+c.takeoff();
 
-client.takeoff();
-
-client
-	.after(5000, function () {
-		this.clockwise(0.5);
-	})
-	.after(3000, function () {
-		this.animate('flipLeft', 15);
-	})
-	.after(1000, function () {
-		this.stop();
-		this.land();
-	});
+c.after(5000, function () {
+	this.clockwise(0.5);
+}).after(3000, function () {
+	this.animate('flipLeft', 15);
+}).after(1000, function () {
+	this.stop();
+	this.land();
+});
